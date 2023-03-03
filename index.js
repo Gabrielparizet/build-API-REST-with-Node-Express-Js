206b2f9977e36ed70c706ceba7c3a3760f6a7f23
Gabrielparizet <parizetgabriel@gmail.com>
2023-03-03 14:25:05 +0100

    Commit before starting reservations methods

--- index.js
+++ index.js
@@ -34,4 +34,11 @@ app.put('/parkings/:id', (req, res) => {
     res.status(200).json(parkings);
 });
 
+app.delete('/parkings/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    let parking = parkings.find(parking => parking.id === id);
+    parkings.splice(parkings.indexOf(parking), 1);
+    res.status(200).json(parkings);
+});
+
 app.listen(8080, () => {console.log('Server listening.')});
