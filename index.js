8254f99e6970dd6e3b7a876cdf58249d2c31aa07
Gabrielparizet <parizetgabriel@gmail.com>
2023-03-03 17:08:55 +0100

    Commit after creating GET route for reservations

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const parkings = require('./parkings.json');
+const reservations = require('./reservations.json');
 
 // app.get('/parkings', (req, res) => {
 //     res.send("Liste des parkings");
@@ -10,21 +11,25 @@ const parkings = require('./parkings.json');
 // Middleware
 app.use(express.json());
 
+// GET/parkings
 app.get('/parkings', (req, res) => {
     res.status(200).json(parkings);
 });
 
+// GET/parkings/:id
 app.get('/parkings/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const parking  = parkings.find(parking => parking.id === id);
     res.status(200).json(parking);
 });
 
+// POST/parkings
 app.post('/parkings', (req, res) => {
     parkings.push(req.body);
     res.status(200).json(parkings);
 });
 
+// PUT/parkings/:id
 app.put('/parkings/:id', (req, res) => {
     const id = parseInt(req.params.id);
     let parking = parkings.find(parking => parking.id === id);
@@ -34,11 +39,45 @@ app.put('/parkings/:id', (req, res) => {
     res.status(200).json(parkings);
 });
 
+// DELETE/parkings
 app.delete('/parkings/:id', (req, res) => {
     const id = parseInt(req.params.id);
     let parking = parkings.find(parking => parking.id === id);
     parkings.splice(parkings.indexOf(parking), 1);
     res.status(200).json(parkings);
 });
+
+
+
+// Reservations
+
+// GET/parkings/:id/reservations
+app.get('/parkings/:id/reservations', (req, res) => {
+    const id = parseInt(req.params.id);
+    const reservation = reservations.find(reservation => reservation.parkingId === id);
+    res.status(200).json(reservation);
+});
+
+// GET/parkings/:id/reservations/:idReservations
+app.get('/parkings/:id/reservations/:idReservations', (req, res) => {
+    const id = parseInt(req.params.id);
+    const idReservations = parseInt(req.params.idReservations);
+    const reservation = reservations.find(reservation => reservation.parkingId === id && reservation.id === idReservations);
+    res.status(200).json(reservation);
+});
+
+// POST/parkings/:id/reservations
+app.post('/parkings/:id/reservations', (req, res) => {
+    const id = parseInt(req.params.id);
+    reservation = reservations.find(reservation => reservation.parkingId === id);
+    reservations.push(req.body);
+    res.status(200).json(reservations);
+});
+
+
+
+
+
+
 
 app.listen(8080, () => {console.log('Server listening.')});
